4a6b2e205d0be904f00b6847a0efc98c2880ce1b
wallmenis <sandboxgamergr@hotmail.com>
2022-12-06 12:25:45 +0200

    added support for 64bit

--- buildscript.sh
+++ buildscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t quartus .
-xhost +localhost
-docker run -it --rm --env="DISPLAY=host.docker.internal:0" --name quartus-container quartus
+docker build --platform linux/amd64 -t quartus .
+#xhost +localhost
+docker run --platform linux/amd64 -it --rm --env="DISPLAY=host.docker.internal:0" --name quartus-container quartus
